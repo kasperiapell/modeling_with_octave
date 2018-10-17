fec3b3127d4e02551a361aedee978a8ead256633
kasperiapell <kasperi.apell@gmail.com>
2018-10-17 10:09:35 +0300

    Oct 17, 2018

---
+++ projection.m
@@ -0,0 +1,9 @@
+% Custom function for a projection of x = (1, 1, 1) 
+% into vector space S spanned by x_1 = (1, 2, 3) and x_2 = (1, 1, -1).
+% In other words, finds the point in S that is closest in Euclidean
+% distance to the point x = (1, 1, 1).
+
+function y = projection(X, x)
+ P = X * inv(X' * X) * X'; 
+ y = P * x; 
+end 
